2538321376512472d6ee2afd1c66e77f61014007
HaroldObasi <haroldobasi2k16@gmail.com>
2022-07-13 21:20:32 +0100

    Testing repo connection

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-const express = require("express")
-const dotenv = require('dotenv/config')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-var morgan = require('morgan')
+const express = require("express");
+const dotenv = require("dotenv/config");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+var morgan = require("morgan");
 
-const port = process.env.PORT
-mongoURI = process.env.MONGO_URI
-const app = express()
+const port = process.env.PORT;
+mongoURI = process.env.MONGO_URI;
+const app = express();
 
-const adminRoutes = require('./routes/adminRoute')
-const photoRoutes = require('./routes/photoRoutes')
+const adminRoutes = require("./routes/adminRoute");
+const photoRoutes = require("./routes/photoRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
-mongoose.connect(mongoURI, { useUnifiedTopology: true,  useNewUrlParser: true })
-.then(() => {console.log("DATABASE CONNECTION SUCCESSFUL",)})
+mongoose
+  .connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log("DATABASE CONNECTION SUCCESSFUL");
+  });
 
 //MIDDLE WARES
 // app.use(bodyParser.json())
-app.use(express.json({limit: '25mb'}));
-app.use(express.urlencoded({limit: '25mb'}));
-app.use(cors())
-app.use(morgan('tiny'))
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
+app.use(cors());
+app.use(morgan("tiny"));
 
-app.use('/admin', adminRoutes)
-app.use('/photos', photoRoutes)
+app.use("/admin", adminRoutes);
+app.use("/photos", photoRoutes);
+app.use("/messages", messageRoutes);
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("testing production");
+});
 
 app.listen(port, () => {
-    console.log("Server started on port ", port)
-})
+  console.log("Server started on port ", port);
+});
